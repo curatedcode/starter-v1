aa1ca17bdca3d3f72d04cfc7efa7e00aca35d007
Zackary Fotheringham <zackaryfotheringham@hotmail.com>
2025-04-21 09:51:28 -0400

    add all initial layout and styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
+
+import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	vite: {
+		// @ts-ignore
+		plugins: [tailwindcss()],
+	},
+
+	integrations: [alpinejs()],
+});
--- src/components/Icons/index.ts
+++ src/components/Icons/index.ts
@@ -1,4 +1,33 @@
 import Bars3Icon from "./Bars3Icon.astro";
+import CalendarDaysIcon from "./CalendarDaysIcon.astro";
+import ChatBubbleLeftIcon from "./ChatBubbleLeftIcon.astro";
+import CheckBadgeIcon from "./CheckBadgeIcon.astro";
+import DocumentCheckIcon from "./DocumentCheckIcon.astro";
+import HandshakeIcon from "./HandshakeIcon.astro";
+import HeartIcon from "./HeartIcon.astro";
+import HomeIcon from "./HomeIcon.astro";
+import MailIcon from "./MailIcon.astro";
+import MapPinIcon from "./MapPinIcon.astro";
+import PhoneIcon from "./PhoneIcon.astro";
+import ScaleIcon from "./ScaleIcon.astro";
+import ScrollPaperIcon from "./ScrollPaperIcon.astro";
+import TrophyIcon from "./TrophyIcon.astro";
 import XMarkIcon from "./XMarkIcon.astro";
 
-export { Bars3Icon, XMarkIcon };
+export {
+	Bars3Icon,
+	XMarkIcon,
+	PhoneIcon,
+	ScaleIcon,
+	ScrollPaperIcon,
+	HomeIcon,
+	HeartIcon,
+	TrophyIcon,
+	CalendarDaysIcon,
+	ChatBubbleLeftIcon,
+	DocumentCheckIcon,
+	HandshakeIcon,
+	MapPinIcon,
+	MailIcon,
+	CheckBadgeIcon,
+};
